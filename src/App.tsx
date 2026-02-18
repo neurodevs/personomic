2ee68b70304e3d819a01be3a6df330988a3fafbe
ericthecurious <hello@ericthecurious.com>
2026-02-17 16:29:55 -0800

    patch: demo StreamMonitor

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { StreamMonitor } from '@neurodevs/react-biosensors'
 import './App.css'
 
 const App: React.FC = () => {
@@ -5,6 +6,7 @@ const App: React.FC = () => {
   return (
     <>
       Hello Personomic!
+      <StreamMonitor streams={[{ name: 'EEG', wssPort: 8080 }, { name: 'PPG', wssPort: 8081 }]}/>
     </>
   )
 }
